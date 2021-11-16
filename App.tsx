1147fae91b9372f1ae9f5b6ff3b26d5baae9e7b9
dja1716 <hdum@uwaterloo.ca>
2021-11-16 13:36:53 +0900

    nav base

--- App.tsx
+++ App.tsx
@@ -3,18 +3,22 @@ import { Text, View } from "react-native";
 import styled from "styled-components/native";
 import AvatarUri from "./components/AvatarUri";
 import Loader from "./components/Loader";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <Container>
-      <AvatarUri
-        size={300}
-        source={
-          "https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com/uploads/-340b910c-1b94-400f-80ac-14f39d454bc4-1636351789614-%EC%BA%A1%EC%B2%98.JPG"
-        }
-      />
-      <Loader size={"large"} />
-    </Container>
+    <NavigationContainer>
+      <Container>
+        <AvatarUri
+          size={300}
+          source={
+            "https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com/uploads/-340b910c-1b94-400f-80ac-14f39d454bc4-1636351789614-%EC%BA%A1%EC%B2%98.JPG"
+          }
+        />
+        <Loader />
+      </Container>
+    </NavigationContainer>
   );
 }
 
