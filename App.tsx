bec67f4cebd4579785bc20349660d73fec576356
dja1716 <hdum@uwaterloo.ca>
2021-11-16 13:13:00 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
+import AvatarUri from "./components/AvatarUri";
+
+export default function App() {
+  return (
+    <Container>
+      <AvatarUri
+        size={300}
+        source={
+          "https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com/uploads/-340b910c-1b94-400f-80ac-14f39d454bc4-1636351789614-%EC%BA%A1%EC%B2%98.JPG"
+        }
+      />
+    </Container>
+  );
+}
+
+// TODO: delete Container after comonentify
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
