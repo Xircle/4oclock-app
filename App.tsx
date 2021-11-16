a26f11fd67dd2690b5e64d224d48f1eb85916915
dja1716 <hdum@uwaterloo.ca>
2021-11-16 15:48:31 +0900

    fetch: app nav basic logic

--- App.tsx
+++ App.tsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
 import AvatarUri from "./components/AvatarUri";
 import Loader from "./components/Loader";
 import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
+import AppLoading from "expo-app-loading";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { AsyncStorage } from "react-native";
+import LoggedOutNav from "./navigators/LoggedOutNav";
+import LoggedInNav from "./navigators/LoggedInNav";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const preloadAssets = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    // const imagesToLoad = [require("./assets/logo.png")];
+    // const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises]);
+  };
+  const preload = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      // isLoggedInVar(true);
+      // tokenVar(token);
+    }
+
+    return preloadAssets();
+  };
+  if (loading) return <AppLoading />;
   return (
+    // @ts-ignore
     <NavigationContainer>
-      <Container>
-        <AvatarUri
-          size={300}
-          source={
-            "https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com/uploads/-340b910c-1b94-400f-80ac-14f39d454bc4-1636351789614-%EC%BA%A1%EC%B2%98.JPG"
-          }
-        />
-        <Loader />
-      </Container>
+      {true ? <LoggedInNav /> : <LoggedOutNav />}
     </NavigationContainer>
   );
 }
