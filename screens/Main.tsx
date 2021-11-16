6613495e3d71a4679b6c14089142ed53d87e6e55
dja1716 <hdum@uwaterloo.ca>
2021-11-16 19:27:09 +0900

    fetch: styles

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components/native";
 import React from "react";
 import XLButton, { ButtonText } from "../components/XLButton";
 import {} from "react-native";
+import { colors } from "../styles/styles";
 
 interface Props {}
 
@@ -9,7 +10,7 @@ export default function Main(props: Props) {
   return (
     <Container>
       <Text>Main</Text>
-      <XLButton bgColor="#18A0FB" onPress={() => console.log("hi")}>
+      <XLButton bgColor={colors.mainBlue} onPress={() => console.log("hi")}>
         <ButtonText>가입하기</ButtonText>
       </XLButton>
     </Container>
---
+++ styles/styles.ts
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export const colors = {
+  black: "#12121D",
+  mainBlue: "#18A0FB",
+};
