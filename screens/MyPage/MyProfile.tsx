ca07c9675a7d81f7992c5e0e3fb4fcdec0cdf1d7
dja1716 <hdum@uwaterloo.ca>
2021-11-19 15:36:55 +0900

    fetch MyProfile with btn and style

--- screens/MyPage/MyProfile.tsx
+++ screens/MyPage/MyProfile.tsx
@@ -1,18 +1,24 @@
 import styled from "styled-components/native";
 import React from "react";
 import { colors } from "../../styles/styles";
+import MainButtonWBg from "../../components/UI/MainButtonWBg";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native";
+import AvatarUri from "../../components/UI/AvatarUri";
 
 interface Props {}
 
 export default function MyProfile(props: Props) {
   return (
     <Container>
-      <Text>MyProfile</Text>
+      <ScrollView></ScrollView>
+      <MainButtonWBg></MainButtonWBg>
     </Container>
   );
 }
 
 const Container = styled.View`
+  position: relative;
   flex: 1;
   justify-content: center;
   align-items: center;
