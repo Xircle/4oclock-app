db47a6c20df2b14b69af0a94f6a5515a40907652
dja1716 <hdum@uwaterloo.ca>
2021-11-19 19:06:50 +0900

    select vars

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,6 +13,34 @@ export const getTOKEN = () => {
 import moment from "moment";
 import "moment/locale/ko";
 
+export const MBTIs = [
+  "ISFP (호기심 많은 예술가)",
+  "ISFJ (용감한 수호자)",
+  "ISTP (만능재주꾼)",
+  "ISTJ (청렴결백 논리주의자)",
+  "INFP (열정적인 중재자)",
+  "INFJ (선의의 옹호자)",
+  "INTP (논리적 사색가)",
+  "INTJ (용의주도한 전략가)",
+  "ESFP (자유영혼 연예인)",
+  "ESFJ (사교적인 외교관)",
+  "ESTP (모험을 즐기는 사업가)",
+  "ESTJ (엄격한 관리자)",
+  "ENFP (재기발랄한 활동가)",
+  "ENFJ (정의로운 사회운동가)",
+  "ENTP (논쟁을 즐기는 변론가)",
+  "ENTJ (대담한 통솔가)",
+  "모름",
+];
+
+export const DrinkingStyles = [
+  "안마셔요",
+  "가끔",
+  "술은 분위기상",
+  "메뉴가 좋으면 못 참지!",
+  "술은 인생의 동반자",
+];
+
 const DateByMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const StartDays = [
   "월요일",
