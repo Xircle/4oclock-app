d0d4e6959114290eb26428581636478fcc760d38
dja1716 <hdum@uwaterloo.ca>
2021-11-19 14:39:19 +0900

    fetch: getMyPlaces API

--- lib/api/getMyPlaces.ts
+++ lib/api/getMyPlaces.ts
@@ -1,10 +1,13 @@
 import { GetMyPlaceOutput, MyPlaceData } from "./types.d";
 import AxiosClient from "../apiClient";
 import { Alert } from "react-native";
+import { BASE_URL } from "../utils";
 
 export const getMyPlaces = async (): Promise<MyPlaceData[]> => {
   const axiosclient = await AxiosClient();
-  const { data } = await axiosclient.get<GetMyPlaceOutput>("user/history");
+  const { data } = await axiosclient.get<GetMyPlaceOutput>(
+    `${BASE_URL}/user/history`
+  );
   if (!data.ok) {
     Alert.alert(data.error);
   }
