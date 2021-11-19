794c1e6303ac7d3f9bd77ba9e869ee6d7467a462
dja1716 <hdum@uwaterloo.ca>
2021-11-19 14:38:20 +0900

    delete: unused components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import React, { useState, useRef } from "react";
-import { SafeAreaView, Text, View } from "react-native";
-import styled from "styled-components/native";
-import AvatarUri from "./components/UI/AvatarUri";
-import Loader from "./components/UI/Loader";
-import { NavigationContainer, ThemeProvider } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
 import AppLoading from "expo-app-loading";
-import { Ionicons } from "@expo/vector-icons";
-import * as Font from "expo-font";
-import { Asset } from "expo-asset";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import LoggedInNav from "./navigators/LoggedInNav";
 import Realm from "realm";
 import { DBContext, UserSchema } from "./lib/RealmDB";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { createStackNavigator } from "@react-navigation/stack";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
 
