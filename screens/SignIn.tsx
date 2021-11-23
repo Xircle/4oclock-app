a1344eb8c9985d0b6b5c4d66c6cbcb703d57ae63
dja1716 <hdum@uwaterloo.ca>
2021-11-23 21:47:43 +0900

    fetch: kakao data fetched correctly

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Text } from "../styles/styles";
 import { RouteProp } from "@react-navigation/native";
 import { LoggedOutStackParamList } from "../navigators/LoggedOutNav";
+import AvatarUri from "../components/UI/AvatarUri";
 
 interface Props {
   route: RouteProp<LoggedOutStackParamList, "SignIn">;
@@ -12,7 +13,9 @@ export default function SignIn({ route }: Props) {
   console.log(route);
   return (
     <Container>
+      <AvatarUri source={route.params.profileImageUrl} size={100} />
       <Text>{route.params.gender}</Text>
+      <Text>{route.params.uid}</Text>
     </Container>
   );
 }
