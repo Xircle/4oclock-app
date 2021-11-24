73a2b518518274869f129002eefef48a7795181c
dja1716 <hdum@uwaterloo.ca>
2021-11-24 14:02:17 +0900

    fetch: Auth step 1 done

--- components/auth/AuthPhoneNumber.tsx
+++ components/auth/AuthPhoneNumber.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components/native";
-import { colors } from "../../styles/styles";
+import {
+  BigTextInput,
+  colors,
+  GreyInfoText,
+  MainHeading,
+} from "../../styles/styles";
 import { AuthAction, AuthState } from "./types";
 import React from "react";
 
@@ -21,11 +26,40 @@ export default function AuthPhoneNumber({ onNext, state, dispatch }: Props) {
       dispatch({ type: "setStage1Valid", payload: true });
     }
   };
-  return <Container></Container>;
+  return (
+    <Container>
+      <MainHeading style={{ marginTop: 40 }}>
+        친구들과 {"\n"}맛있는 밥 먹으러 갈까요?
+      </MainHeading>
+      <GreyInfoText style={{ marginTop: 20 }}>
+        모임 단통방을 만들어드리는 용도로 사용되기에 꼭! 사용하시는 전화번호를
+        적어주셔야 해요.
+      </GreyInfoText>
+      <PhoneNumberInput
+        blurOnSubmit={true}
+        onSubmitEdition={onNext}
+        returnKeyType="next"
+        returnKeyLabel="next"
+        autoCapitalize="none"
+        autoCorrect={false}
+        autoFocus={true}
+        keyboardType="number-pad"
+      />
+    </Container>
+  );
 }
 
 const Container = styled.View`
   flex: 1;
   background-color: ${colors.bgColor};
   padding: 15px;
+`;
+
+const PhoneNumberInput = styled(BigTextInput)`
+  margin-top: 50px;
+  height: 50px;
+  border-radius: 0px;
+  border: none;
+  border-bottom-width: 0.5px;
+  border-color: ${colors.bareGrey};
 `;
