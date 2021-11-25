8baed6cbb3a066353a8db26bdcc60e2f9a46ef81
dja1716 <hdum@uwaterloo.ca>
2021-11-25 18:51:10 +0900

    fetch: Avartar component now can load local image (anonymous)

--- components/UI/AvatarUri.tsx
+++ components/UI/AvatarUri.tsx
@@ -1,13 +1,24 @@
 import styled from "styled-components/native";
 import React from "react";
+import { Asset, useAssets } from "expo-asset";
+import { View } from "react-native";
 
 interface Props {
-  source: string;
+  source?: string;
   size: number;
 }
 
 export default function AvatarUri({ size, source }: Props) {
-  return <AvatarImage source={{ uri: source }} size={size} />;
+  const [assets] = useAssets([
+    require("../../statics/images/anonymous_user.png"),
+  ]);
+  if (!assets?.[0]) {
+    return <View></View>;
+  } else {
+    return (
+      <AvatarImage source={source ? { uri: source } : assets[0]} size={size} />
+    );
+  }
 }
 
 const AvatarImage = styled.Image<{ size: number }>`
