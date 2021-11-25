5193d9f97da8afdb25bb322d1f19d1309ae91b58
dja1716 <hdum@uwaterloo.ca>
2021-11-25 18:50:35 +0900

    fetch: profile image upload

--- components/auth/AuthProfileImage.tsx
+++ components/auth/AuthProfileImage.tsx
@@ -1,8 +1,19 @@
 import styled from "styled-components/native";
-import React from "react";
-import { View } from "react-native";
-import { colors, Text } from "../../styles/styles";
+import React, { useState } from "react";
+import { Alert, View } from "react-native";
+import {
+  colors,
+  fontFamilies,
+  GeneralText,
+  GreyInfoText,
+  MainHeading,
+  Text,
+} from "../../styles/styles";
 import { AuthAction, AuthState } from "./types.d";
+import AvatarUri from "../UI/AvatarUri";
+import * as ImagePicker from "react-native-image-picker";
+import { authDispatcher } from "../../lib/auth/AuthDispatcher";
+import { authValidation } from "../../lib/auth/AuthValidation";
 
 interface Props {
   onNext: () => void;
@@ -11,9 +22,55 @@ interface Props {
 }
 
 export default function AuthProfileImage({ onNext, state, dispatch }: Props) {
+  const [loading, setLoading] = useState(false);
+
+  const fileHandle = async () => {
+    const option: ImagePicker.ImageLibraryOptions = {
+      mediaType: "photo",
+      includeBase64: true,
+    };
+    const result = await ImagePicker.launchImageLibrary(option);
+    //console.log(result);
+    if (result.errorMessage) {
+      return Alert.alert(result.errorMessage);
+    } else if (!result.didCancel && result?.assets?.[0].uri) {
+      if (result.assets?.[0].fileSize > 10000000) {
+        return Alert.alert(
+          "사진 최대 용량을 초과했습니다. 사진 용량은 최대 10MB입니다."
+        );
+      } else {
+        const file = {
+          type: "image/jpeg",
+          uri: result.assets[0].uri,
+          name: result.assets[0].fileName,
+        };
+        authDispatcher.dispatchProfileImg(file, result.assets[0].uri, dispatch);
+
+        // setLocalProfileData((prev) => ({
+        //   ...prev,
+        //   profileImageFile: file,
+        // }));
+        // setLocalProfileData((prev) => ({
+        //   ...prev,
+        //   profileImageUrl: result.assets[0].uri,
+        // }));
+      }
+    }
+  };
+
   return (
-    <Container>
-      <Text>4</Text>
+    <Container showsVerticalScrollIndicator={false}>
+      <MainHeading style={{ marginTop: 20 }}>마지막! 프로필 사진</MainHeading>
+      <InfoText>개성을 나타나는 사진을 업로드 해주세요</InfoText>
+      <AvatarContainer onPress={fileHandle}>
+        <AvatarUri size={100} source={state.profileImgUrl} />
+        <AvatarText>프로필 사진 업로드하기</AvatarText>
+      </AvatarContainer>
+      <MainInstText># 프로필 사진 예시</MainInstText>
+      <SubInstText>
+        관심사를 즐기는 사진 / 좋아하는 공간에서 찍은 사진 / 라이프 스타일이
+        보여지는 사진 / 모임을 즐기고 있는 사진
+      </SubInstText>
     </Container>
   );
 }
@@ -21,4 +78,31 @@ export default function AuthProfileImage({ onNext, state, dispatch }: Props) {
 const Container = styled.ScrollView`
   background-color: ${colors.bgColor};
   padding: 15px;
+`;
+const AvatarContainer = styled.TouchableOpacity`
+  align-items: center;
+  margin: 20px;
+`;
+const InfoText = styled(GreyInfoText)`
+  margin-top: 18px;
+`;
+
+const AvatarText = styled(GeneralText)`
+  color: ${colors.mainBlue};
+  font-family: ${fontFamilies.bold};
+  margin-top: 12px;
+`;
+
+const MainInstText = styled(GeneralText)`
+  color: ${colors.midGrey};
+  font-family: ${fontFamilies.bold};
+  margin-top: 16px;
+`;
+
+const SubInstText = styled(GeneralText)`
+  color: ${colors.bareGrey};
+  font-family: ${fontFamilies.medium};
+  margin-top: 15px;
+  font-size: 13px;
+  line-height: 17px;
 `;
