2e12cec3cf3be392cd077d1907b9aa842fa6b05e
dja1716 <hdum@uwaterloo.ca>
2021-11-26 16:58:21 +0900

    fetch: Main > Top Carousel linear gradient

--- components/main/TopCarouselPlace.tsx
+++ components/main/TopCarouselPlace.tsx
@@ -1,21 +1,23 @@
 import styled from "styled-components/native";
-import { colors, GeneralText, Text } from "../../styles/styles";
+import { colors, fontFamilies, GeneralText, Text } from "../../styles/styles";
 import React from "react";
-import { Dimensions } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
 
 interface Props {
   coverImageUrl: string;
   width: number;
   height: number;
   key: number;
+  name: string;
 }
 
 export default function TopCarouselPlace({
   coverImageUrl,
   width,
   height,
+  name,
 }: Props) {
-  console.log("tc");
   return (
     <Container>
       <CoverImage
@@ -23,16 +25,46 @@ export default function TopCarouselPlace({
         width={width}
         height={height}
       />
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["transparent", colors.black]}
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          borderRadius: 15,
+        }}
+      />
+      <TextContainer>
+        <NameText>{name}</NameText>
+      </TextContainer>
     </Container>
   );
 }
 
+const TextContainer = styled.View`
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
+`;
+
+const NameText = styled(GeneralText)`
+  color: ${colors.bgColor};
+  font-family: ${fontFamilies.bold};
+  font-size: 20px;
+`;
+
 const Container = styled.View`
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
 
 const CoverImage = styled.Image<{ width: number; height: number }>`
   width: ${(props) => props.width + "px"};
   height: ${(props) => props.height + "px"};
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
 `;
