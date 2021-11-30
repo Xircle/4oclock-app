b6080a87a7c7ab3d7a99abb43b8a4d09c0a5ba30
dja1716 <hdum@uwaterloo.ca>
2021-11-30 16:51:52 +0900

    fetch: create place

--- components/activity/CreatePlaceStage1.tsx
+++ components/activity/CreatePlaceStage1.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components/native";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
+  BigTextInput,
   BlackLabel,
   colors,
   MainHeading,
   SubHeading,
+  TextArea,
 } from "../../styles/styles";
 import { ScrollView, View, Animated, Dimensions, Platform } from "react-native";
 import ExpandableV from "../UI/ExpandableV";
@@ -17,8 +19,20 @@ const { width } = Dimensions.get("window");
 export default function CreatePlaceStage1(props: Props) {
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
+  const [maxParticiapnts, setMaxParticipants] = useState(2);
 
-  console.log(width);
+  const changeMaxParticipants = (plus: boolean) => {
+    if (maxParticiapnts > 2 && !plus) {
+      setMaxParticipants((prev) => prev - 1);
+    }
+    if (maxParticiapnts < 8 && plus) {
+      setMaxParticipants((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
   return (
     <Container>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -27,14 +41,33 @@ export default function CreatePlaceStage1(props: Props) {
           재밌는 모임을 열어보자~~ 행복하고 재밌는 모임
         </SubHeading>
 
-        <ExpandableV title="어떤 모임인가요? (제목)" height={100}>
-          <MainHeading>모임을 열어볼까?</MainHeading>
+        <ExpandableV title="어떤 모임인가요? (제목)" height={80}>
+          <InnerContainer>
+            <SBigTextInput
+              placeholder="ex. 말이 많아요 / 치믈리에 새내기..."
+              autoCapitalize="none"
+              blurOnSubmit={true}
+              returnKeyType="next"
+              returnKeyLabel="next"
+              autoCorrect={false}
+            />
+          </InnerContainer>
         </ExpandableV>
-        <ExpandableV title="모임에 대한 간단한 소개!" height={100}>
-          <MainHeading>모임을 열어볼까?</MainHeading>
+        <ExpandableV title="모임에 대한 간단한 소개!" height={130}>
+          <InnerContainer style={[{ paddingTop: 15 }, { paddingBottom: 15 }]}>
+            <STextArea
+              placeholder="ex. 안녕하세요 트와이스를 좋아하는 인문대생입니다 / 취준하느라 너무 힘들어요...! 같이 고민이야기 하실 분!"
+              autoCapitalize="none"
+              blurOnSubmit={true}
+              returnKeyType="next"
+              returnKeyLabel="next"
+              autoCorrect={false}
+              multiline={true}
+            />
+          </InnerContainer>
         </ExpandableV>
-        <ExpandableV title="만남시간" height={100}>
-          <AnimInnerContainer>
+        <ExpandableV title="만남시간" height={80}>
+          <InnerContainer>
             <PickerContainer onPress={() => setOpen(true)}>
               <WhiteText>시간 선택하기</WhiteText>
             </PickerContainer>
@@ -51,17 +84,36 @@ export default function CreatePlaceStage1(props: Props) {
                 setOpen(false);
               }}
             />
-          </AnimInnerContainer>
+          </InnerContainer>
         </ExpandableV>
-        <ExpandableV title="만남장소" height={100}>
-          <MainHeading>모임을 열어볼까?</MainHeading>
+        <ExpandableV title="만남장소" height={80}>
+          <InnerContainer>
+            <SBigTextInput
+              placeholder="ex. 말이 많아요 / 치믈리에 새내기..."
+              autoCapitalize="none"
+              blurOnSubmit={true}
+              returnKeyType="next"
+              returnKeyLabel="next"
+              autoCorrect={false}
+            />
+          </InnerContainer>
         </ExpandableV>
         <ExpandableV title="최대 참가인원" height={100}>
           <MainHeading>모임을 열어볼까?</MainHeading>
         </ExpandableV>
-        <ExpandableV title="참가비" height={100}>
-          <MainHeading>모임을 열어볼까?</MainHeading>
+        <ExpandableV title="참가비" height={80}>
+          <InnerContainer>
+            <SBigTextInput
+              placeholder="ex. 말이 많아요 / 치믈리에 새내기..."
+              autoCapitalize="none"
+              blurOnSubmit={true}
+              returnKeyType="next"
+              returnKeyLabel="next"
+              autoCorrect={false}
+            />
+          </InnerContainer>
         </ExpandableV>
+        <View style={{ height: 150 }} />
       </ScrollView>
     </Container>
   );
@@ -84,12 +136,14 @@ const Container = styled.View`
   padding: 0px 30px;
 `;
 
-const AnimContainer = styled.View`
-  width: 100%;
+const SBigTextInput = styled(BigTextInput)``;
+
+const InnerContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
-const AnimInnerContainer = styled.View`
-  margin-top: 20px;
-  flex-direction: row;
-  justify-content: space-around;
+const STextArea = styled(TextArea)`
+  flex: 1;
 `;
--- components/activity/CreatePlaceStage2.tsx
+++ components/activity/CreatePlaceStage2.tsx
@@ -2,7 +2,15 @@ import styled from "styled-components/native";
 import React from "react";
 import { TouchableOpacity, ScrollView } from "react-native";
 import MyBackButton from "../UI/MyBackButton";
-import { colors, MainHeading, SubHeading } from "../../styles/styles";
+
+import { Ionicons } from "@expo/vector-icons";
+import {
+  BlackLabel,
+  colors,
+  GeneralText,
+  MainHeading,
+  SubHeading,
+} from "../../styles/styles";
 
 interface Props {
   onBackPressed: () => void;
@@ -19,6 +27,14 @@ export default function CreatePlaceStage2({ onBackPressed }: Props) {
         <SubHeading style={{ marginTop: 20, marginBottom: 20 }}>
           재밌는 모임을 열어보자~~ 행복하고 재밌는 모임
         </SubHeading>
+
+        <SBlackLabel>관련 사진 올리기 (3개 이상)</SBlackLabel>
+        <AddPhotoContiner>
+          <AddPhotoWrapper>
+            <Ionicons name="camera-outline" size={42} color="#A7B0C0" />
+            <AddPhotoText>사진추가</AddPhotoText>
+          </AddPhotoWrapper>
+        </AddPhotoContiner>
       </ScrollView>
     </Container>
   );
@@ -27,4 +43,25 @@ export default function CreatePlaceStage2({ onBackPressed }: Props) {
 const Container = styled.View`
   flex: 1;
   padding: 0px 30px;
+`;
+
+const SBlackLabel = styled(BlackLabel)``;
+
+const AddPhotoContiner = styled.TouchableOpacity`
+  margin-top: 22px;
+`;
+
+const AddPhotoWrapper = styled.View`
+  width: 100%;
+  background-color: #f9f9f9;
+  height: 200px;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const AddPhotoText = styled(GeneralText)`
+  margin-left: 20px;
+  font-size: 18px;
 `;
