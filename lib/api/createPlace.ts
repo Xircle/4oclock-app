2fcd773f316c8d970608b6fad9e9501aedefb4a2
dja1716 <hdum@uwaterloo.ca>
2021-12-01 12:34:10 +0900

    fetch: create place api

--- lib/api/createPlace.ts
+++ lib/api/createPlace.ts
@@ -8,30 +8,23 @@ export const createPlace = async (
   const formData = new FormData();
   const axiosclient = await AxiosClient();
 
-  formData.append("coverImage", placeData.coverImageFile!);
+  formData.append("coverImage", placeData.coverImage!);
 
-  if (placeData.reviewImagesFile) {
-    for (let i = 0; i < placeData.reviewImagesFile.length; i++) {
-      formData.append("reviewImages", placeData.reviewImagesFile[i]!);
+  if (placeData.subImages) {
+    for (let i = 0; i < placeData.subImages.length; i++) {
+      formData.append("subImages", placeData.subImages[i]!);
     }
   }
-  formData.append("reviewDescription", placeData.reviewDescription);
   formData.append("name", placeData.name);
-  formData.append("isLightning", placeData.isLightning + "");
   if (placeData.maxParticipantsNumber)
-    formData.append("maxParticipantsNumber", placeData.maxParticipantsNumber);
-  formData.append("location", placeData.location);
-  formData.append("detailLink", placeData.detailLink);
+    formData.append(
+      "maxParticipantsNumber",
+      placeData.maxParticipantsNumber + ""
+    );
   formData.append("detailAddress", placeData.detailAddress);
-
-  formData.append("categories", JSON.stringify(placeData.categories));
   formData.append("description", placeData.description);
-  formData.append("title", placeData.title);
-  formData.append("startTime", placeData.startTime);
   formData.append("startDateAt", placeData.startDateAt + "");
-  formData.append("recommendation", placeData.recommendation);
   formData.append("participationFee", placeData.participationFee);
-  formData.append("oneLineIntroText", placeData.oneLineIntroText);
   const { data } = await axiosclient.post<CreateActivityOutput>(
     `${BASE_URL}/place`,
     formData
