dbbc108778f70df317fcbaa627898bb6bc9dcefa
dja1716 <hdum@uwaterloo.ca>
2021-12-16 15:41:07 +0900

    rework:component  full screen loader

--- components/UI/FullScreenLoader.tsx
+++ components/UI/FullScreenLoader.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components/native";
 import React from "react";
 import { colors } from "../../styles/styles";
 
-interface Props {}
+interface Props {
+  notFullScreen?: boolean;
+}
 
 const { width, height } = Dimensions.get("window");
 
-export default function FullScreenLoader(props: Props) {
+export default function FullScreenLoader({ notFullScreen }: Props) {
   return (
     <Container>
       <ActivityIndicator color={colors.mainBlue} size="large" />
@@ -15,9 +17,9 @@ export default function FullScreenLoader(props: Props) {
   );
 }
 
-const Container = styled.View`
+const Container = styled.View<{ notFullScreen?: boolean }>`
   position: absolute;
-  width: ${width + "px"};
+  width: ${(props) => (props.notFullScreen ? "100%" : width + "px")};
   height: 100%;
   background-color: rgba(255, 255, 255, 0.5);
   justify-content: center;
