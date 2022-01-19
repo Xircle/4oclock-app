24ca4f140703540358b1fc27d189a735bf1a9483
dja1716 <hdum@uwaterloo.ca>
2022-01-19 17:17:47 +0900

    fetch: dimensions screen -> window

--- components/auth/AuthProfileMainData.tsx
+++ components/auth/AuthProfileMainData.tsx
@@ -27,7 +27,7 @@ interface Props {
   dispatch: React.Dispatch<AuthAction>;
 }
 
-const { width } = Dimensions.get("screen");
+const { width } = Dimensions.get("window");
 
 export default function AuthProfileMainData({
   onNext,
