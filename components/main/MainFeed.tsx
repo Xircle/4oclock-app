efd634d10457ec9e107d258ddeed29b87c62034b
dja1716 <hdum@uwaterloo.ca>
2022-03-28 19:40:27 +0900

    feat: 2.5기 enhance performance by memoization

---
+++ components/main/MainFeed.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components/native";
+import React from "react";
+import { OptimizedFlatList } from "react-native-optimized-flatlist";
+import { PlaceFeedData } from "../../lib/api/types";
+import { colors } from "../../styles/styles";
+
+interface Props {
+  loadMore: () => void;
+  onRefresh: () => void;
+  refreshing: boolean;
+  places: PlaceFeedData[];
+  renderItem: ({ item }: { item: any }) => JSX.Element;
+  listHeaderCompoent: JSX.Element;
+}
+
+function MainFeed({
+  loadMore,
+  onRefresh,
+  refreshing,
+  places,
+  renderItem,
+  listHeaderCompoent,
+}: Props) {
+  const placeFlatlistKeyExtractor = (item: PlaceFeedData, index) =>
+    item.id + "" + index;
+  return (
+    <AnimWrapper
+      disableVirtualization={false}
+      showsVerticalScrollIndicator={false}
+      onEndReached={loadMore}
+      onEndReachedThreshold={0.2}
+      onRefresh={onRefresh}
+      refreshing={refreshing}
+      keyExtractor={placeFlatlistKeyExtractor}
+      // @ts-ignore
+      data={places}
+      renderItem={renderItem}
+      ListHeaderComponent={listHeaderCompoent}
+    />
+  );
+}
+
+export default React.memo(MainFeed);
+
+const AnimWrapper = styled(OptimizedFlatList)`
+  background-color: ${colors.bgColor};
+  width: 100%;
+  height: 100%;
+  padding: 15px 20px;
+`;
