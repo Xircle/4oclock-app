c59740aabac40d91168a2d66276400bf8b872870
dja1716 <hdum@uwaterloo.ca>
2022-04-12 11:58:23 +0900

    feat: delete account api

---
+++ lib/api/deleteAccount.ts
@@ -0,0 +1,14 @@
+import { CoreOutput } from "./types.d";
+import AxiosClient from "../apiClient";
+import { AuthState } from "../../components/auth/types";
+import { BASE_URL } from "../utils";
+
+export const createAccount = async (state: AuthState): Promise<CoreOutput> => {
+  const formData = new FormData();
+  const axiosclient = await AxiosClient();
+
+  const { data } = await axiosclient.delete<CoreOutput>(
+    `${BASE_URL}/user/delete`
+  );
+  return data;
+};
