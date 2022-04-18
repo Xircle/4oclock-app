97bc5bca9c7db4c1c93ae1732375d5c96b72fb19
dja1716 <hdum@uwaterloo.ca>
2022-04-18 11:37:54 +0900

    top tab components base

---
+++ screens/ActivitiesManagement/CreatedActivityScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/native";
+
+type Props = {};
+
+const CreatedActivityScreen = (props: Props) => {
+  return <Container></Container>;
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #28d27d;
+`;
+
+export default CreatedActivityScreen;
---
+++ screens/ActivitiesManagement/ParticipatingActivityScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/native";
+
+type Props = {};
+
+const ParticipatingActivityScreen = (props: Props) => {
+  return <Container></Container>;
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #d23128;
+`;
+
+export default ParticipatingActivityScreen;
