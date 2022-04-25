a348f9c112e92aa39542d7580415e50921317676
dja1716 <hdum@uwaterloo.ca>
2022-04-25 16:40:29 +0900

    clean up: deleteAccount

--- lib/api/deleteAccount.ts
+++ lib/api/deleteAccount.ts
@@ -1,10 +1,8 @@
 import { CoreOutput } from "./types.d";
 import AxiosClient from "../apiClient";
-import { AuthState } from "../../components/auth/types";
 import { BASE_URL } from "../utils";
 
 export const deleteAccount = async (): Promise<CoreOutput> => {
-  const formData = new FormData();
   const axiosclient = await AxiosClient();
 
   const { data } = await axiosclient.delete<CoreOutput>(
