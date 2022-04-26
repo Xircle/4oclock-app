29d4d88b715d5cb8b998398010088e7133fc76b0
dja1716 <hdum@uwaterloo.ca>
2022-04-26 14:58:42 +0900

    feat: edit place api base

---
+++ lib/api/editPlace.ts
@@ -0,0 +1,47 @@
+import { ActivityState } from "./../activity/ActivityReducer";
+import { CreateActivityOutput } from "./types.d";
+import AxiosClient from "../apiClient";
+import { BASE_URL } from "../utils";
+import { typeKoToEn } from "./createPlace";
+
+export const createPlace = async (
+  placeData: ActivityState,
+  placeId: string
+): Promise<CreateActivityOutput> => {
+  const formData = new FormData();
+  const axiosclient = await AxiosClient();
+
+  formData.append("coverImage", placeData.coverImage!);
+
+  if (placeData.subImages) {
+    for (let i = 0; i < placeData.subImages.length; i++) {
+      formData.append("subImages", placeData.subImages[i]!);
+    }
+  }
+  formData.append("name", placeData.name);
+  if (placeData.maxParticipantsNumber) {
+    formData.append("maxParticipantsNumber", placeData.maxParticipantsNumber);
+  }
+  // BEGIN:for testing
+  if (placeData.recommendation) {
+    formData.append("recommendation", placeData.recommendation);
+  }
+  // END
+  formData.append("detailAddress", placeData.detailAddress);
+  formData.append("description", placeData.description);
+  formData.append("startDateAt", placeData.startDateAt + "");
+  formData.append("participationFee", placeData.participationFee);
+  formData.append("placeId", placeData.placeId);
+  formData.append("isVaccinated", true);
+  formData.append("placeType", typeKoToEn[placeData.activityType]);
+  formData.append("kakaoLink", placeData.kakaoLink);
+  if (typeKoToEn[placeData.activityType] === "Regular-meeting") {
+    formData.append("team", placeData.team);
+  }
+
+  const { data } = await axiosclient.post<CreateActivityOutput>(
+    `${BASE_URL}/place/${placeId}`,
+    formData
+  );
+  return data;
+};
