cb54a9817d55b5e44b211a3ce5e6d0e490be2946
dja1716 <hdum@uwaterloo.ca>
2022-04-27 14:31:50 +0900

    edit: get My Places Created API

--- lib/api/getMyPlacesCreated.ts
+++ lib/api/getMyPlacesCreated.ts
@@ -1,11 +1,16 @@
-import { GetMyPlaceOutput, MyPlaceData } from "./types.d";
+import {
+  GetMyCreatedPlaceOutput,
+  GetMyPlaceOutput,
+  MyCreatedPlaceData,
+  MyPlaceData,
+} from "./types.d";
 import AxiosClient from "../apiClient";
 import { Alert } from "react-native";
 import { BASE_URL } from "../utils";
 
-export const getMyPlacesCreated = async (): Promise<MyPlaceData[]> => {
+export const getMyPlacesCreated = async (): Promise<MyCreatedPlaceData[]> => {
   const axiosclient = await AxiosClient();
-  const { data } = await axiosclient.get<GetMyPlaceOutput>(
+  const { data } = await axiosclient.get<GetMyCreatedPlaceOutput>(
     `${BASE_URL}/user/history/created`
   );
   if (!data.ok) {
