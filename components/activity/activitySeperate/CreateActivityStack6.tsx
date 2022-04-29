6bb875e8db8ce3083b849cdc9628dad17feee60d
dja1716 <hdum@uwaterloo.ca>
2022-04-29 18:17:34 +0900

    edit: CAS6

--- components/activity/activitySeperate/CreateActivityStack6.tsx
+++ components/activity/activitySeperate/CreateActivityStack6.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components/native";
-import React from "react";
+import React, { useEffect } from "react";
 import { MainHeading } from "../../../styles/styles";
 import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 interface Props {}
 
 export default function CreateActivityStack6(props: Props) {
+  const navigation = useNavigation();
+  useEffect(() => {
+    // @ts-ignore
+    navigation.navigate("CreatePlaceT");
+  });
   return (
     <Container>
       <TouchableOpacity>
