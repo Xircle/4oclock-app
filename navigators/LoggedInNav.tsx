bbdb69d1d045c5b35e48a22df25fe3c66b7ad810
dja1716 <hdum@uwaterloo.ca>
2022-05-02 19:43:27 +0900

    feat: logigedin nav > placeId

--- navigators/LoggedInNav.tsx
+++ navigators/LoggedInNav.tsx
@@ -35,6 +35,7 @@ export type LoggedInStackParamList = {
     participantsData: ParticipantsData;
     participants: PlaceDataParticipantsProfile[];
     isCreator: boolean;
+    placeId: string;
   };
 };
 
