c2bd19e86e3b18ec5d48134cecd3ab261a2c75da
dja1716 <hdum@uwaterloo.ca>
2022-05-02 16:59:36 +0900

    edit: ParticipantsList > participants count

--- screens/ActivitiesDetail/ParticipantsList.tsx
+++ screens/ActivitiesDetail/ParticipantsList.tsx
@@ -2,7 +2,7 @@ import { RouteProp } from "@react-navigation/native";
 import React, { useEffect } from "react";
 import styled from "styled-components/native";
 import { LoggedInStackParamList } from "../../navigators/LoggedInNav";
-import { colors, MainHeading } from "../../styles/styles";
+import { colors, fontFamilies, GeneralText } from "../../styles/styles";
 
 interface Props {
   route: RouteProp<LoggedInStackParamList, "ParticipantsList">;
@@ -15,7 +15,11 @@ export default function ParticipantsList({ route }: Props) {
 
   return (
     <Container showsVerticalScrollIndicator={false}>
-      <Heading>ParticipantsList</Heading>
+      <Heading>{route.params.placeName}</Heading>
+      <AgeContainer>
+        <AgeText>남 {route.params.participantsData.maleCount}</AgeText>
+        <AgeText>여 {route.params.participantsData.femaleCount}</AgeText>
+      </AgeContainer>
     </Container>
   );
 }
@@ -23,6 +27,22 @@ export default function ParticipantsList({ route }: Props) {
 const Container = styled.ScrollView`
   flex: 1;
   background-color: ${colors.bgColor};
+  padding: 15px;
 `;
 
-const Heading = styled(MainHeading)``;
+const Heading = styled(GeneralText)`
+  font-family: ${fontFamilies.medium};
+  font-size: 26px;
+`;
+
+const AgeContainer = styled.View`
+  flex-direction: row;
+  margin-top: 30px;
+`;
+
+const AgeText = styled(GeneralText)`
+  margin-right: 10px;
+  font-size: 20px;
+  font-family: ${fontFamilies.medium};
+  color: ${colors.midGrey};
+`;
