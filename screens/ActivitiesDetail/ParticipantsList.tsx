98f9f00fcbd4b6e106f3620ef226a28bdfbab013
dja1716 <hdum@uwaterloo.ca>
2022-05-02 16:20:14 +0900

    edit: get route props successfully

--- screens/ActivitiesDetail/ParticipantsList.tsx
+++ screens/ActivitiesDetail/ParticipantsList.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { View } from "react-native";
+import { RouteProp } from "@react-navigation/native";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
-import { ParticipantsData } from "../../lib/api/types";
-import { MainHeading } from "../../styles/styles";
+import { LoggedInStackParamList } from "../../navigators/LoggedInNav";
+import { colors, MainHeading } from "../../styles/styles";
 
-type Props = {
-  participants: ParticipantsData[];
-};
+interface Props {
+  route: RouteProp<LoggedInStackParamList, "ParticipantsList">;
+}
 
-export default function ParticipantsList({}: Props) {
+export default function ParticipantsList({ route }: Props) {
+  useEffect(() => {
+    console.log(route.params.participants);
+  }, []);
+
   return (
     <Container showsVerticalScrollIndicator={false}>
       <Heading>ParticipantsList</Heading>
@@ -16,6 +20,9 @@ export default function ParticipantsList({}: Props) {
   );
 }
 
-const Container = styled.ScrollView``;
+const Container = styled.ScrollView`
+  flex: 1;
+  background-color: ${colors.bgColor};
+`;
 
 const Heading = styled(MainHeading)``;
