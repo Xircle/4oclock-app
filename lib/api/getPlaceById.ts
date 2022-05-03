73e841b55db70b207678c9e314f0cd6b1ae39a96
dja1716 <hdum@uwaterloo.ca>
2022-05-03 17:41:17 +0900

    clean up: getPlaceById api

--- lib/api/getPlaceById.ts
+++ lib/api/getPlaceById.ts
@@ -1,12 +1,11 @@
 import { GetPlaceByIdOutput, PlaceData } from "./types.d";
 import AxiosClient from "../apiClient";
-import storage from "../helpers/myAsyncStorage";
 import { BASE_URL } from "../utils";
 
 export const getPlaceById = async (placeId: string): Promise<PlaceData> => {
   const axiosclient = await AxiosClient();
   const { data } = await axiosclient.get<GetPlaceByIdOutput>(
-    `${BASE_URL}/place/${placeId}`,
+    `${BASE_URL}/place/${placeId}`
   );
   if (!data.ok) {
     throw new Error(data.error);
