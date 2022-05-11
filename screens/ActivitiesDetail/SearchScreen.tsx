a622c0e5b92754965f16dba1703482e34ca3568d
dja1716 <hdum@uwaterloo.ca>
2022-05-11 17:53:49 +0900

    clean up

--- screens/ActivitiesDetail/SearchScreen.tsx
+++ screens/ActivitiesDetail/SearchScreen.tsx
@@ -1,11 +1,8 @@
 import styled from "styled-components/native";
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
 
 interface Props {}
 
 export default function SearchScreen({}: Props) {
-  const navigation = useNavigation();
-
   return <></>;
 }
