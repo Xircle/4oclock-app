f78181d5d0971ba44cefe0b79e5d9efd6c4ef47a
dja1716 <hdum@uwaterloo.ca>
2022-05-30 20:05:33 +0900

    refactor: app.tsx

--- App.tsx
+++ App.tsx
@@ -1,67 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import "react-native-gesture-handler";
-import AppLoading from "expo-app-loading";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import LoggedInNav from "./navigators/LoggedInNav";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Alert, Image, LogBox } from "react-native";
-import { Asset } from "expo-asset";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./lib/reducers";
-import { Provider } from "react-redux";
+import { requestUserPermission } from "./lib/firebase/messaging";
+import { useMutation } from "react-query";
+import { updateFirebaseToken } from "./lib/api/updateFirebaseToken";
 
 //LogBox.ignoreLogs(["Setting a timer for a long period of time"]);
+
 const Stack = createStackNavigator();
-const queryClient = new QueryClient();
-
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
-const store = configureStore({ reducer: rootReducer });
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-
-  const startLoading = async () => {
-    const images = loadImages([
-      require("./statics/images/anonymous_user.png"),
-      require("./statics/images/landingPageImage.png"),
-      require("./statics/images/RegularHeader.jpeg"),
-    ]);
-    await Promise.all([...images]);
-  };
-  const onFinish = async () => {
-    setReady(true);
-  };
-
-  if (!ready)
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{ headerShown: false, gestureEnabled: false }}
-          >
-            <Stack.Screen name="LoggedOutNav" component={LoggedOutNav} />
-            <Stack.Screen name="LoggedInNav" component={LoggedInNav} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </QueryClientProvider>
-    </Provider>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false, gestureEnabled: false }}
+      >
+        <Stack.Screen name="LoggedOutNav" component={LoggedOutNav} />
+        <Stack.Screen name="LoggedInNav" component={LoggedInNav} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
