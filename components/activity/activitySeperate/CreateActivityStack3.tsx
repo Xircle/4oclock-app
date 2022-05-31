455eae8faf1e9ec46db79c42aa7b216cd1b63ea3
dja1716 <hdum@uwaterloo.ca>
2022-05-31 19:18:53 +0900

    edit: CAS3

--- components/activity/activitySeperate/CreateActivityStack3.tsx
+++ components/activity/activitySeperate/CreateActivityStack3.tsx
@@ -5,10 +5,10 @@ import {
   colors,
   ErrorMessage,
   MainHeading,
+  SpaceBetweenWrapper,
   TextArea,
 } from "../../../styles/styles";
 import MyKeyboardAvoidingView from "../../UI/MyKeyboardAvoidingView";
-import MainButtonWBg from "../../UI/MainButtonWBg";
 import { ScrollView } from "react-native-gesture-handler";
 import { activityDispatcher } from "../../../lib/activity/ActivityDispatcher";
 import { useNavigation } from "@react-navigation/native";
@@ -17,6 +17,7 @@ import { RootState } from "../../../lib/reducers";
 import { useSelector } from "react-redux";
 import { activityValidation } from "../../../lib/activity/CreateActivityValidation";
 import { createPlaceErrorMessage } from "../../../lib/errorMessages";
+import RelativeMainButtonWBg from "../../UI/RelativeMainButtonWBG";
 
 interface Props {}
 
@@ -35,39 +36,42 @@ export default function CreateActivityStack3(props: Props) {
 
   return (
     <MyKeyboardAvoidingView keyboardVerticalOffset={50}>
-      <CASContainer>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <MainHeading>어떤 활동을 하는 모임이야?</MainHeading>
+      <SpaceBetweenWrapper>
+        <CASContainer>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <MainHeading>어떤 활동을 하는 모임이야?</MainHeading>
 
-          <STextArea
-            placeholder="모임에 대한 설명을 입력해줘! 함께하고 싶은 주제나 내용을 입력하면 좋아"
-            autoCapitalize="none"
-            returnKeyType="next"
-            returnKeyLabel="next"
-            autoCorrect={false}
-            multiline={true}
-            defaultValue={description ? description : ""}
-            onChange={(event) => {
-              const { eventCount, target, text } = event.nativeEvent;
-              activityDispatcher.dispatchDescription(text, dispatch);
-              setDescriptionError(
-                !activityValidation.validateDescription(text)
-              );
-            }}
-            error={descriptionError}
-          />
-          {descriptionError ? (
-            <SErrorMessage>{createPlaceErrorMessage[1]}</SErrorMessage>
-          ) : null}
-        </ScrollView>
-      </CASContainer>
+            <STextArea
+              placeholder="모임에 대한 설명을 입력해줘! 함께하고 싶은 주제나 내용을 입력하면 좋아"
+              autoCapitalize="none"
+              returnKeyType="next"
+              returnKeyLabel="next"
+              autoCorrect={false}
+              multiline={true}
+              defaultValue={description ? description : ""}
+              onChange={(event) => {
+                const { eventCount, target, text } = event.nativeEvent;
+                activityDispatcher.dispatchDescription(text, dispatch);
+                setDescriptionError(
+                  !activityValidation.validateDescription(text)
+                );
+              }}
+              error={descriptionError}
+            />
+            {descriptionError ? (
+              <SErrorMessage>{createPlaceErrorMessage[1]}</SErrorMessage>
+            ) : null}
+          </ScrollView>
+        </CASContainer>
 
-      {/* @ts-ignore */}
-      <MainButtonWBg
-        onPress={nextHandler}
-        disabled={!description}
-        title={"다음"}
-      />
+        {/* @ts-ignore */}
+        <RelativeMainButtonWBg
+          onPress={nextHandler}
+          disabled={!description}
+          title={"다음"}
+          bottom={10}
+        />
+      </SpaceBetweenWrapper>
     </MyKeyboardAvoidingView>
   );
 }
