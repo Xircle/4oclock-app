4d38d6bb9118b931b85bfe09bc496924f8a8dec3
dja1716 <hdum@uwaterloo.ca>
2022-06-08 14:53:06 +0900

    rename: VerificationSend -> sendVerfication

--- lib/api/sendVerification.ts
+++ lib/api/sendVerification.ts
@@ -3,7 +3,7 @@ import { AxiosResponse } from "axios";
 import AxiosClient from "../apiClient";
 import { BASE_URL } from "../utils";
 
-export const sendReservation = async (
+export const sendVerification = async (
   sendVerificationInput: VerificationSendInput
 ): Promise<AxiosResponse<VerificationSendOutput>> => {
   const axiosclient = await AxiosClient();
