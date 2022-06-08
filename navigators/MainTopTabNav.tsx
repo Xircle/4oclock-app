0a41a90476126e6cb11087eacc68ac736033835f
dja1716 <hdum@uwaterloo.ca>
2022-06-08 18:04:51 +0900

    modify: main top tab nav

---
+++ navigators/MainTopTabNav.tsx
@@ -0,0 +1,22 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import CreatedActivityScreen from "../screens/ActivitiesManagement/CreatedActivityScreen";
+import ParticipatingActivityScreen from "../screens/ActivitiesManagement/ParticipatingActivityScreen";
+import React from "react";
+import { fontFamilies } from "../styles/styles";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function MainTopTabNav() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontFamily: fontFamilies.regular,
+        },
+      }}
+    >
+      <Tab.Screen name="참가" component={ParticipatingActivityScreen} />
+      <Tab.Screen name="생성" component={CreatedActivityScreen} />
+    </Tab.Navigator>
+  );
+}
