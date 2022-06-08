b36f5ce909530279ef6c0d8ef354abc6b60d5054
dja1716 <hdum@uwaterloo.ca>
2022-06-08 16:47:06 +0900

    clean up: apiClient

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,12 +1,6 @@
 import axios from "axios";
 import storage from "./helpers/myAsyncStorage";
 
-// http://localhost:3080
-const host =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_PRODUCTION_API_SERVER
-    : process.env.REACT_APP_PRODUCTION_API_SERVER;
-
 const giveApiClient = async () => {
   const token = await storage.getItem("token");
   const targetApiClient = axios.create({
