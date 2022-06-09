422bec614c7ceed0bdd8d96d150988399ec74165
dja1716 <hdum@uwaterloo.ca>
2022-06-09 14:15:05 +0900

    edit: MainRegularTab > on isYkClub

--- components/main/MainRegularTab.tsx
+++ components/main/MainRegularTab.tsx
@@ -1,8 +1,9 @@
 import React, { useMemo, useState } from "react";
-import { useInfiniteQuery, useQueryClient } from "react-query";
+import { useInfiniteQuery, useQuery, useQueryClient } from "react-query";
 import styled from "styled-components/native";
 import { getPlacesRegular } from "../../lib/api/getPlaces";
-import { GetPlacesByLocationOutput } from "../../lib/api/types";
+import { getUser } from "../../lib/api/getUser";
+import { GetPlacesByLocationOutput, UserData } from "../../lib/api/types";
 import { colors, GeneralText } from "../../styles/styles";
 import MainFeed from "./MainFeed";
 import { renderRegular } from "./MainRenderItems";
@@ -12,6 +13,12 @@ interface Props {}
 function MainRegularTab(props: Props) {
   const [refreshing, setRefreshing] = useState(false);
   const queryClient = useQueryClient();
+
+  const { data: userData, refetch: refetchUserData } = useQuery<
+    UserData | undefined
+  >(["userProfile"], () => getUser(), {
+    retry: 1,
+  });
 
   const {
     data: mainRegularData,
@@ -45,6 +52,7 @@ function MainRegularTab(props: Props) {
     () => renderRegular,
     [mainRegularData?.pages?.map((page) => page.places).flat()]
   );
+
   return (
     <Container>
       <MainFeed
@@ -54,12 +62,18 @@ function MainRegularTab(props: Props) {
         renderItem={memoizedValueRegular}
         places={mainRegularData?.pages?.map((page) => page.places).flat()}
         listHeaderCompoent={
-          <LightningInfoContainer>
-            <LightningInfoText>
-              (매우중요) 정기모임 참여는 마이페이지 {">"} 프로필 수정하기에서
-              팀과 활동코드를 입력해 주셔야지만 가능해요!
-            </LightningInfoText>
-          </LightningInfoContainer>
+          userData.isYkClub ? (
+            <RegularInfoContainer>
+              <RegularInfoText>
+                (매우중요) 정기모임 참여는 마이페이지 {">"} 프로필 수정하기에서
+                팀과 활동코드를 입력해 주셔야지만 가능해요!
+              </RegularInfoText>
+            </RegularInfoContainer>
+          ) : (
+            <RegularInfoContainer>
+              <RegularInfoText>호롤롤롤</RegularInfoText>
+            </RegularInfoContainer>
+          )
         }
       />
     </Container>
@@ -73,9 +87,9 @@ const Container = styled.View`
   flex: 1;
 `;
 
-const LightningInfoContainer = styled.View``;
+const RegularInfoContainer = styled.View``;
 
-const LightningInfoText = styled(GeneralText)`
+const RegularInfoText = styled(GeneralText)`
   line-height: 24px;
   color: ${colors.midGrey};
   padding-left: 10px;
