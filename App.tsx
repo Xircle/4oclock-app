ff8d4cec0dbaeea9f57d235637d5fdb175107610
dja1716 <hdum@uwaterloo.ca>
2022-06-10 21:05:34 +0900

    edit: app

--- App.tsx
+++ App.tsx
@@ -1,18 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import LoggedInNav from "./navigators/LoggedInNav";
 import { createStackNavigator } from "@react-navigation/stack";
 import { requestUserPermission } from "./lib/firebase/messaging";
+import messaging from "@react-native-firebase/messaging";
+import { useMutation } from "react-query";
+import { updateFirebaseToken } from "./lib/api/updateFirebaseToken";
 
 //LogBox.ignoreLogs(["Setting a timer for a long period of time"]);
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const { mutateAsync: mutateUpdateFirebaseToken } =
+    useMutation(updateFirebaseToken);
   useEffect(() => {
     requestUserPermission();
+
+    messaging()
+      .getToken()
+      .then((token) => {
+        mutateUpdateFirebaseToken(token);
+      });
   }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
