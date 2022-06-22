040efb21c6a44c58b3607fb327ed01c727c5ef92
dja1716 <hdum@uwaterloo.ca>
2022-06-22 15:21:14 +0900

    new: verifyByCode API

---
+++ lib/api/verifyByCode.ts
@@ -0,0 +1,12 @@
+import { CoreOutput } from "./types.d";
+import AxiosClient from "../apiClient";
+import { BASE_URL } from "../utils";
+
+export const verifyByCode = async (code: string): Promise<CoreOutput> => {
+  const axiosclient = await AxiosClient();
+
+  const { data } = await axiosclient.post<CoreOutput>(
+    `${BASE_URL}/user/me/${code}`
+  );
+  return data;
+};
