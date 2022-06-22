c89410fce8ee7204762979a83db009ac1e2397ed
dja1716 <hdum@uwaterloo.ca>
2022-06-22 15:46:47 +0900

    fix: updateFirebaseToken when there is no userToken

--- lib/api/updateFirebaseToken.ts
+++ lib/api/updateFirebaseToken.ts
@@ -1,10 +1,15 @@
 import { CoreOutput } from "./types.d";
 import AxiosClient from "../apiClient";
 import { BASE_URL } from "../utils";
+import storage from "../helpers/myAsyncStorage";
 
 export const updateFirebaseToken = async (
   token: string
 ): Promise<CoreOutput> => {
+  const userToken = await storage.getItem("token");
+  if (!userToken) {
+    return { ok: false, error: "no userToken. Which is fine" };
+  }
   const axiosclient = await AxiosClient();
 
   const { data } = await axiosclient.post<CoreOutput>(
