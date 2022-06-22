830d0a13a15e5aed9a5b488ea1d0c3254a0fc62b
dja1716 <hdum@uwaterloo.ca>
2022-06-22 22:17:55 +0900

    edit: patchTeam api

--- lib/api/patchTeam.ts
+++ lib/api/patchTeam.ts
@@ -5,7 +5,7 @@ import { BASE_URL } from "../utils";
 export const patchTeam = async (teamId: string): Promise<CoreOutput> => {
   const axiosclient = await AxiosClient();
 
-  const { data } = await axiosclient.post<CoreOutput>(
+  const { data } = await axiosclient.patch<CoreOutput>(
     `${BASE_URL}/user/me/team/${teamId}`
   );
   return data;
