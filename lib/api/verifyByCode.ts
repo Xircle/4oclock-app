dd3ac324ab53686d7bb671e04ac68af9f482bfb3
dja1716 <hdum@uwaterloo.ca>
2022-06-22 22:18:13 +0900

    edit: verifyByCode api

--- lib/api/verifyByCode.ts
+++ lib/api/verifyByCode.ts
@@ -5,8 +5,9 @@ import { BASE_URL } from "../utils";
 export const verifyByCode = async (code: string): Promise<CoreOutput> => {
   const axiosclient = await AxiosClient();
 
-  const { data } = await axiosclient.post<CoreOutput>(
-    `${BASE_URL}/user/me/verify/${code}`
+  const { data } = await axiosclient.patch<CoreOutput>(
+    `${BASE_URL}/user/me/verify/${code}`,
+    {}
   );
   return data;
 };
