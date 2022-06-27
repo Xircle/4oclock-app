b535b0f0bd8b28182ed6b8214bdd5330938f6187
dja1716 <hdum@uwaterloo.ca>
2022-06-27 19:47:22 +0900

    edit: CASPlaceTypeScreen

--- components/activity/activitySeperate/CASPlaceTypeScreen.tsx
+++ components/activity/activitySeperate/CASPlaceTypeScreen.tsx
@@ -2,13 +2,13 @@ import styled from "styled-components/native";
 import React, { useEffect } from "react";
 import { CreateActivityStackParamList } from "../../../navigators/CreateActivityStackNav";
 import { useNavigation } from "@react-navigation/native";
-import { colors, MainHeading } from "../../../styles/styles";
-import { TouchableOpacity } from "react-native";
+import { colors, MainHeading, SubHeading } from "../../../styles/styles";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../lib/reducers";
 import { activityDispatcher } from "../../../lib/activity/ActivityDispatcher";
 import { useDispatch } from "react-redux";
 import CreatePlaceTypeSelector from "../CreatePlaceTypeSelector";
+import RelativeMainButtonWBg from "../../UI/RelativeMainButtonWBG";
 
 type Props = CreateActivityStackParamList["CASPlaceType"];
 
@@ -33,23 +33,30 @@ export default function CASPlaceTypeScreen(props: Props) {
   };
 
   return (
-    <Container>
+    <SpaceBetweenWrapper>
+      <Container>
+        <MainHeading>모임을 열어볼까?</MainHeading>
+        <SubHeading style={{ marginTop: 20, marginBottom: 20 }}>
+          재밌는 모임을 열어볼까? 열고 친구들과 꿀잼모임😊
+        </SubHeading>
+        <InnerContainer style={{ paddingBottom: 5 }}>
+          <CreatePlaceTypeSelector
+            onPress={setActivityType}
+            selectedType={activityType}
+          />
+        </InnerContainer>
+      </Container>
       {/* @ts-ignore */}
-      <TouchableOpacity onPress={() => navigation.navigate("CAS3")}>
-        <MainHeading>Finish</MainHeading>
-      </TouchableOpacity>
-      <InnerContainer style={{ paddingBottom: 5 }}>
-        <CreatePlaceTypeSelector
-          onPress={setActivityType}
-          selectedType={activityType}
-        />
-      </InnerContainer>
-    </Container>
+      <RelativeMainButtonWBg onPress={nextHandler} title={"다음"} bottom={10} />
+    </SpaceBetweenWrapper>
   );
 }
 
-const Container = styled.View`
+const Container = styled.ScrollView`
   flex: 1;
+  padding-top: 20px;
+  padding-left: 15px;
+  padding-right: 15px;
   background-color: ${colors.bgColor};
 `;
 
@@ -57,4 +64,10 @@ const InnerContainer = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+`;
+
+export const SpaceBetweenWrapper = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  width: 100%;
 `;
