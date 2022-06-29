c271bc227ea952f8218698686ca2affa93347645
dja1716 <hdum@uwaterloo.ca>
2022-06-29 15:02:19 +0900

    clean-up: index

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import Setup from "./Setup";
-import messaging from "@react-native-firebase/messaging";
-import storage from "./lib/helpers/myAsyncStorage";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
