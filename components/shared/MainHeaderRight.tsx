3fab990058026a5c1e1ab5a9ec8f29866f8df7ad
dja1716 <hdum@uwaterloo.ca>
2022-06-30 16:58:43 +0900

    edit: MainHeaderRight

---
+++ components/shared/MainHeaderRight.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { colors, MyAlert } from "../../styles/styles";
+import { Ionicons } from "@expo/vector-icons";
+import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
+import messaging from "@react-native-firebase/messaging";
+import { NotificationData } from "../../lib/api/types";
+import storage, { StorageKey } from "../../lib/helpers/myAsyncStorage";
+
+type Props = {};
+
+const MainHeaderRight = (props: Props) => {
+  const [notificationReceived, setNotificationReceived] = useState(false);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    let mounted = true;
+    messaging().onMessage(async (remoteMessage) => {
+      if (remoteMessage.data?.type !== "message") setNotificationReceived(true);
+    });
+    async function fetchNotification() {
+      if (mounted) {
+        const data: NotificationData = await storage.getItem(
+          StorageKey.notifications
+        );
+        if (!data) return;
+        if (data?.unreadNotifications?.length) {
+          setNotificationReceived(true);
+        }
+      }
+    }
+    async function cleanUp() {
+      mounted = false;
+    }
+    fetchNotification();
+    return () => {
+      cleanUp();
+    };
+  }, []);
+  function CTA() {
+    setNotificationReceived(false);
+    // @ts-ignore
+    navigation.navigate("NotificationScreen");
+  }
+  return (
+    <TouchableOpacity
+      style={{ marginRight: 8 }}
+      // @ts-ignore
+      onPress={CTA}
+    >
+      {notificationReceived && <NotificationAlert />}
+      <Ionicons name="notifications-outline" size={30} color={colors.black} />
+    </TouchableOpacity>
+  );
+};
+
+export default MainHeaderRight;
+
+const NotificationAlert = styled(MyAlert)`
+  top: 0px;
+  left: -5px;
+`;
