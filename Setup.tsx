278777b655a5e982381440f5a52dcacdb641c4d9
dja1716 <hdum@uwaterloo.ca>
2022-06-30 12:49:08 +0900

    refactor: Setup - messaging

--- Setup.tsx
+++ Setup.tsx
@@ -3,16 +3,13 @@ import App from "./App";
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Image } from "react-native";
+import { Alert, Image } from "react-native";
 import { Asset } from "expo-asset";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./lib/reducers";
 import { Provider } from "react-redux";
-import messaging, {
-  FirebaseMessagingTypes,
-} from "@react-native-firebase/messaging";
-import storage from "./lib/helpers/myAsyncStorage";
-import { openLink } from "./components/shared/Links";
+import messaging from "@react-native-firebase/messaging";
+import { notificationHandler } from "./lib/firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCPaFLT9I2OPjvrS-HKvks1nzvFquaeeKw",
@@ -29,41 +26,8 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-async function notificationHandler(
-  remoteMessage: FirebaseMessagingTypes.RemoteMessage
-) {
-  if (remoteMessage.data?.type === "message") return;
-  const CTA = async () => {
-    if (remoteMessage.data?.type === "place") {
-    } else if (remoteMessage.data?.type === "okLink") {
-      await openLink.LOpenLink(remoteMessage.data?.okLink);
-    }
-  };
-  const newNotification = {
-    type: remoteMessage.data?.type,
-    CTA: CTA,
-    image: "",
-    title: remoteMessage.notification?.title,
-    body: remoteMessage.notification?.body,
-  };
-  let notifications = await storage.getItem("notifications");
-  if (notifications) {
-    notifications.unreadNotifications.unshift(newNotification);
-  } else {
-    notifications = {
-      unreadNotifications: [newNotification],
-      readNotifications: [],
-    };
-  }
-  await storage.setItem("notifications", notifications);
-}
-
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  notificationHandler(remoteMessage);
-});
-
 messaging().onMessage(async (remoteMessage) => {
-  notificationHandler(remoteMessage);
+  await notificationHandler(remoteMessage);
 });
 
 const queryClient = new QueryClient();
