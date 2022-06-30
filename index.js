be1880b8ed1d9107d7706350377916e9c8e7ea78
dja1716 <hdum@uwaterloo.ca>
2022-06-30 12:48:44 +0900

    edit: index - setBackgroundMessageHandler

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import Setup from "./Setup";
+import messaging from "@react-native-firebase/messaging";
+import { notificationHandler } from "./lib/firebase/messaging";
+import { Alert } from "react-native";
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  await notificationHandler(remoteMessage);
+  Alert.alert("background");
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
