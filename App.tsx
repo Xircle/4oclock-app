b718ee6d374a5619c74e76983003c315fc5ce4d4
dja1716 <hdum@uwaterloo.ca>
2022-11-15 14:42:37 +0900

    App - nav container ref

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import LoggedInNav from "./navigators/LoggedInNav";
 import { createStackNavigator } from "@react-navigation/stack";
+import { navigationRef } from "./lib/RootNavigation";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator
         screenOptions={{ headerShown: false, gestureEnabled: false }}
       >
